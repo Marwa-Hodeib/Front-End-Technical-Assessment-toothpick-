3e4f3b6b6a8d8c64fd05c7b0f6e66c4a8998c3d1
marwa-hodeib <marwahodeib31@gmail.com>
2020-07-31 19:14:38 +0300

    indentation :)

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import './Header.css';
+
+
+const Header = () => (
+    <header className="header-wrapper">
+        
+        <div></div>
+        <div className="header-logo"><img src="/Images/main-logo.svg" alt='logo'/></div>
+        
+
+        <nav className="header-navigation-items">
+            <ul>
+                <li className="header-link"><a href="/">Home</a></li>
+                <li className="header-link"><a href="/">How it Works</a></li>
+                <li className="header-link"><a href="/">About</a></li>
+                <li className="header-link"><a href="/">Download</a></li>
+                <li className="header-link"><a href="/">For Business</a></li>
+                <li className="header-link"><a href="/">Contact</a></li>
+            </ul>
+        </nav>
+
+    </header>
+    );
+
+export default Header;
