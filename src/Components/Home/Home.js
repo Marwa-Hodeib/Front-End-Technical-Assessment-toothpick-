e87f3202a632c2baec7109927d39568610849877
marwa-hodeib <marwahodeib31@gmail.com>
2020-08-01 14:33:19 +0300

    Adding a slider, adding a background image

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,29 +1,81 @@
-import React from 'react';
-import './Home.css';
+import React from "react";
+import Slider from "react-slick";
+import "./Home.css";
 
+const settings = {
+  dots: true,
+  infinite: true,
+  speed: 500,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  adaptiveHeight: true,
+  className: "slider variable-width",
+  centerMode: true,
+  Description: false,
+};
 
 const Home = () => (
-   <div id='home' className='home-wrapper'>
-
+  <div id='home' className='home-wrapper'>
+    <div className='main-container'>
       <div className='home-wrapper-text'>
-
-         <div className='home-title'>
-            The Dental Ecosystem
-         </div>
-         <p className='home-paragraph'>
-            Toothpick allows you to easily find,order and track all of your dental products in 3 easy steps.
-
-         </p>
-         <img className='home-app-image' src='/Images/app-store-black.svg' alt='app store'/>
-         <img className='home-app-image' src='/Images/google-play-black.svg' alt='google play'/>
+        <div className='home-title'>The Dental Ecosystem</div>
+        <p className='home-paragraph'>
+          Toothpick allows you to easily find,order and track all of your dental
+          products in 3 easy steps.
+        </p>
+        <img
+          className='home-app-image'
+          src='/Images/app-store-black.svg'
+          alt='app store'
+        />
+        <img
+          className='home-app-image'
+          src='/Images/google-play-black.svg'
+          alt='google play'
+        />
       </div>
       <div className='home-wrapper-image'>
-         <img className='home-mobile-image' src='/Images/img_hero_1.png' alt='app store'/>
+        <Slider {...settings}>
+          <div className='slide'>
+            <img
+              className='home-mobile-image'
+              src='/Images/img_hero_1.png'
+              alt='app store'
+            />
+          </div>
+
+          <div className='slide'>
+            <img
+              className='home-mobile-image'
+              src='/Images/img_hero_1.png'
+              alt='app store'
+            />
+          </div>
+
+          <div className='slide'>
+            <img
+              className='home-mobile-image'
+              src='/Images/img_hero_1.png'
+              alt='app store'
+            />
+          </div>
+
+          <div className='slide'>
+            <img
+              className='home-mobile-image'
+              src='/Images/img_hero_1.png'
+              alt='app store'
+            />
+          </div>
+        </Slider>
       </div>
-
-
-   </div>
-   
+    </div>
+    <img
+      src='/Images/home-background.svg'
+      className='home-background'
+      alt='home background'
+    />
+  </div>
 );
 
 export default Home;
